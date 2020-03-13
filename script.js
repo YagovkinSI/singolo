a5014180124e67ed5bf56dfd4079898e26f9a334
Сергей Яговкин <scorpionupgrade@mail.ru>
2020-03-13 16:57:10 +0300

    feat: add turning off and on phones on the slider

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-const NAV = document.getElementById("nav__ul");
+const nav = document.getElementById("nav__ul");
 const anchors = document.querySelectorAll('a[href*="#"]')
 
-NAV.addEventListener("click", (event) => {
-    NAV.querySelectorAll("a").forEach(li => li.classList.remove("nav__link_active"));
+var phone_1 = document.getElementById("phone__vertical"); 
+var phone_2 = document.getElementById("phone__horizontal");
+var phone_content_1 = document.getElementById("phone_content_1");
+var phone_content_2 = document.getElementById("phone_content_2");
+
+
+nav.addEventListener("click", (event) => {
+    nav.querySelectorAll("a").forEach(li => li.classList.remove("nav__link_active"));
     event.target.classList.add("nav__link_active");
-    var blockID = anchor.getAttribute('href').substr(1);
-    document.getElementById(blockID).scrollIntoView({
-        behavior: 'smooth',
-        block: 'start'
-    })
 });
+
+phone_1.addEventListener("click", () => { changePhoneContentVisible(phone_content_1) });
+phone_content_1.addEventListener("click", () => { changePhoneContentVisible(phone_content_1) });
+phone_2.addEventListener("click", () => { changePhoneContentVisible(phone_content_2) });
+phone_content_2.addEventListener("click", () => { changePhoneContentVisible(phone_content_2) });
+function changePhoneContentVisible(content) {
+    if (content.classList.contains("invisible")) {
+        content.classList.remove("invisible")
+    }
+    else {
+        content.classList.add("invisible")
+    }
+}
+
